63a1847537a3ef83b4fcaa2caa6f5ce3f384b4e0
Ollie Kemp <oliverkemp1999@gmail.com>
2022-07-28 11:38:32 +0100

    Added documentation for animal_sounds()

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,16 @@
+#' Describes the Given Sound and Animal as a Message
+#'
+#' Takes an animal and a sound as a string, and creates a message stating that the animal says the sound.
+#'
+#' @param animal A character string
+#' @param sound A character string
+#'
+#' @return A message giving the constructed sentence
+#' @export
+#'
+#' @examples
+#' animal_sounds("cow", "moo")
+#' animal_sounds("sheep", "baa")
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
